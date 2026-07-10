cbb2dbb601d745705f07d1d40536eae5ca9b7ee8
Justin-Gomez-DEV <justin.gomez.dev@gmail.com>
2026-07-10 10:59:15 +0100

    Update ChatColor to MiniMessage

--- src/main/java/dev/gomez/java/betterChatFilter/utils/MessageUtil.java
+++ src/main/java/dev/gomez/java/betterChatFilter/utils/MessageUtil.java
@@ -1,10 +1,20 @@
 package dev.gomez.java.betterChatFilter.utils;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 
 public final class MessageUtil {
+
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+
     public static void send(Player player, String message){
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+
+
+        if (message.contains("<#")) {
+            player.sendMessage(MINI_MESSAGE.deserialize(message));
+        } else {
+            player.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(message));
+        }
     }
 }
