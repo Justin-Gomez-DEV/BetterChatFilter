5dc31bae099e6cd3fae86a1688a72034807b72b7
Justin-Gomez-DEV <justin.gomez.dev@gmail.com>
2026-07-12 22:18:08 +0100

    Rework ReloadCommand.java to fit with new Command System

--- src/main/java/dev/gomez/java/betterChatFilter/commands/ReloadCommand.java
+++ src/main/java/dev/gomez/java/betterChatFilter/commands/ReloadCommand.java
@@ -1,36 +1,30 @@
 package dev.gomez.java.betterChatFilter.commands;
 
 import dev.gomez.java.betterChatFilter.config.ConfigManager;
+import dev.gomez.java.betterChatFilter.shared.permissions.EnumPermissions;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
-public class ReloadCommand implements CommandExecutor {
+public class ReloadCommand {
 
-    private ConfigManager configManager;
+    private final ConfigManager configManager;
 
     public ReloadCommand(ConfigManager configManager) {
         this.configManager = configManager;
     }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+    public void execute(CommandSender sender) {
 
-        if(sender.hasPermission("betterchatfilter.reload")) {
-            if(args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-
-                configManager.reload();
-
-                sender.sendMessage(Component.text("[BetterChatFilter] Config reloaded successfully!").color(NamedTextColor.GREEN));
-
-                return true;
-
-            }
+        if (!sender.hasPermission(EnumPermissions.RELOAD.getPermission())) {
+            sender.sendMessage(
+                    Component.text("You do not have permission to use this command.", NamedTextColor.RED));
+            return;
         }
 
-        return false;
+        configManager.reload();
+
+        sender.sendMessage(
+                Component.text("[BetterChatFilter] Config reloaded successfully!", NamedTextColor.GREEN));
     }
 }
